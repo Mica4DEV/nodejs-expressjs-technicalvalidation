888f9363f77b313009b8fd5d2c515e95e3d66f8f
Micaël FELIX <mfelix@cbtw.tech>
2024-07-12 12:08:21 +0200

    feat: example code

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,20 @@ import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
 import * as middlewares from './middlewares';
 import api from './controllers';
 
 require('dotenv').config();
+
+const client = new MongoClient("mongodb+srv://dbUser:dbUserPassword@cbtw-mongo-technical-va.yajbi9r.mongodb.net/?retryWrites=true&w=majority&appName=cbtw-mongo-technical-validation-1", {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
 const app = express();
 
@@ -14,6 +23,7 @@ app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.json({
@@ -22,6 +32,58 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/v1', api);
+
+app.post('/users/search', async (req, res) => {
+  const query = req.body as { email: string };
+
+  const client = new MongoClient("mongodb+srv://dbUser:dbUserPassword@cbtw-mongo-technical-va.yajbi9r.mongodb.net/?retryWrites=true&w=majority&appName=cbtw-mongo-technical-validation-1", {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+  const db = client.db("sample_mflix");
+  const usersCollection = db.collection("users");
+
+  const users = await usersCollection.find(query).toArray();
+
+  if (users) {
+    let usersOutput: any = [];
+    users.forEach(user => {
+      usersOutput.push({
+        email: user.email,
+        name: user.name,
+      });
+    });
+    res.json(usersOutput);
+  } else {
+    res.status(404).send('User not found');
+  }
+  await client.close();
+});
+
+app.post('/users', async (req, res) => {
+  const user = req.body;
+
+  const client = new MongoClient("mongodb+srv://dbUser:dbUserPassword@cbtw-mongo-technical-va.yajbi9r.mongodb.net/?retryWrites=true&w=majority&appName=cbtw-mongo-technical-validation-1", {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+  const db = client.db("sample_mflix");
+  const usersCollection = db.collection("users");
+
+  const result = await usersCollection.insertOne(user);
+
+  if (result.acknowledged) {
+      res.status(201).send('User created');
+  } else {
+      res.status(500).send('Failed to create user');
+  }
+});
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
